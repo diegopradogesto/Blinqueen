931270e70cc7ba3b7f9b71f197f4d0d5dce63170
Diego Prado Gesto <diego.pradogesto@gmail.com>
2020-05-03 18:24:46 +0200

    First version of the app that controls light

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,240 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const {smarthome} = require('actions-on-google');
+const {google} = require('googleapis');
+const util = require('util');
+const admin = require('firebase-admin');
+// Initialize Firebase
+admin.initializeApp();
+const firebaseRef = admin.database().ref('/');
+// Initialize Homegraph
+const auth = new google.auth.GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/homegraph'],
+});
+const homegraph = google.homegraph({
+  version: 'v1',
+  auth: auth,
+});
+
+exports.fakeauth = functions.https.onRequest((request, response) => {
+  const responseurl = util.format('%s?code=%s&state=%s',
+      decodeURIComponent(request.query.redirect_uri), 'xxxxxx',
+      request.query.state);
+  console.log(responseurl);
+  return response.redirect(responseurl);
+});
+
+exports.faketoken = functions.https.onRequest((request, response) => {
+  const grantType = request.query.grant_type ?
+    request.query.grant_type : request.body.grant_type;
+  const secondsInDay = 86400; // 60 * 60 * 24
+  const HTTP_STATUS_OK = 200;
+  console.log(`Grant type ${grantType}`);
+
+  let obj;
+  if (grantType === 'authorization_code') {
+    obj = {
+      token_type: 'bearer',
+      access_token: '123access',
+      refresh_token: '123refresh',
+      expires_in: secondsInDay,
+    };
+  } else if (grantType === 'refresh_token') {
+    obj = {
+      token_type: 'bearer',
+      access_token: '123access',
+      expires_in: secondsInDay,
+    };
+  }
+  response.status(HTTP_STATUS_OK)
+      .json(obj);
+});
+
+const app = smarthome({
+  debug: true,
+});
+
+app.onSync((body) => {
+  return {
+    requestId: body.requestId,
+    payload: {
+      agentUserId: '123',
+      devices: [{
+        id: 'light',
+        type: 'action.devices.types.SENSOR',
+        traits: [
+          'action.devices.traits.OnOff',
+          'action.devices.traits.Brightness'
+        ],
+        name: {
+          defaultNames: ['My Blinqueen LED'],
+          name: 'Blinqueen',
+          nicknames: ['Blinqueen'],
+        },
+        deviceInfo: {
+          manufacturer: 'Acme Co',
+          model: 'acme-blinqueen',
+          hwVersion: '1.0',
+          swVersion: '1.0',
+        },
+        willReportState: true,
+        attributes: {
+          commandOnlyOnOff: false,
+          commandOnlyBrightness: false
+        },
+      }],
+    },
+  };
+});
+
+const queryFirebase = async (deviceId) => {
+  const snapshot = await firebaseRef.child(deviceId).once('value');
+  const snapshotVal = snapshot.val();
+  return {
+    on: snapshotVal.OnOff.on,
+    brightness: snapshotVal.Brightness.brightness,
+  };
+};
+
+const queryDevice = async (deviceId) => {
+  const data = await queryFirebase(deviceId);
+  return {
+    on: data.on,
+    brightness: data.brightness
+  };
+};
+
+app.onQuery(async (body) => {
+  const {requestId} = body;
+  const payload = {
+    devices: {},
+  };
+  const queryPromises = [];
+  const intent = body.inputs[0];
+  for (const device of intent.payload.devices) {
+    const deviceId = device.id;
+    queryPromises.push(queryDevice(deviceId)
+        .then((data) => {
+        // Add response to device payload
+          payload.devices[deviceId] = data;
+        }
+        ));
+  }
+  // Wait for all promises to resolve
+  await Promise.all(queryPromises);
+  return {
+    requestId: requestId,
+    payload: payload,
+  };
+});
+
+const updateDevice = async (execution, deviceId) => {
+  const {params, command} = execution;
+  let state; let ref;
+  switch (command) {
+    case 'action.devices.commands.OnOff':
+      state = {on: params.on};
+      ref = firebaseRef.child(deviceId).child('OnOff');
+      break;
+    case 'action.devices.commands.BrightnessAbsolute':
+      state = {brightness: params.brightness};
+      ref = firebaseRef.child(deviceId).child('Brightness');
+      break;
+  }
+
+  return ref.update(state).then(() => state);
+};
+
+app.onExecute(async (body) => {
+  const {requestId} = body;
+  // Execution results are grouped by status
+  const result = {
+    ids: [],
+    status: 'SUCCESS',
+    states: {
+      online: true,
+    },
+  };
+
+  const executePromises = [];
+  const intent = body.inputs[0];
+  for (const command of intent.payload.commands) {
+    for (const device of command.devices) {
+      for (const execution of command.execution) {
+        executePromises.push(
+            updateDevice(execution, device.id)
+                .then((data) => {
+                  result.ids.push(device.id);
+                  Object.assign(result.states, data);
+                })
+                .catch(() => console.error(`Unable to update ${device.id}`))
+        );
+      }
+    }
+  }
+
+  await Promise.all(executePromises);
+  return {
+    requestId: requestId,
+    payload: {
+      commands: [result],
+    },
+  };
+});
+
+app.onDisconnect((body, headers) => {
+  console.log('User account unlinked from Google Assistant');
+  // Return empty response
+  return {};
+});
+
+exports.smarthome = functions.https.onRequest(app);
+
+exports.requestsync = functions.https.onRequest(async (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  console.info('Request SYNC for user 123');
+  try {
+    const res = await homegraph.devices.requestSync({
+      requestBody: {
+        agentUserId: '123',
+      },
+    });
+    console.info('Request sync response:', res.status, res.data);
+    response.json(res.data);
+  } catch (err) {
+    console.error(err);
+    response.status(500).send(`Error requesting sync: ${err}`);
+  }
+});
+
+
+/**
+ * Send a REPORT STATE call to the homegraph when data for any device id
+ * has been changed.
+ */
+exports.reportstate = functions.database.ref('{deviceId}').onWrite(async (change, context) => {
+  console.info('Firebase write event triggered this cloud function');
+  const snapshot = change.after.val();
+
+  const requestBody = {
+    requestId: 'ff36a3cc', /* Any unique ID */
+    agentUserId: '123', /* Hardcoded user ID */
+    payload: {
+      devices: {
+        states: {
+          /* Report the current state of our light */
+          [context.params.deviceId]: {
+            on: snapshot.OnOff.on,
+            brightness: snapshot.Brightness.brightness
+          },
+        },
+      },
+    },
+  };
+
+  const res = await homegraph.devices.reportStateAndNotification({
+    requestBody,
+  });
+  console.info('Report state response:', res.status, res.data);
+});
